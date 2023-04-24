62cb9ca7b8eb04d8c4890f4845c8201c0ac5874b
ak26252 <akunhokim@gmail.com>
2023-04-24 18:33:57 -0400

    added caliper files and report

---
+++ hyperledger-caliper/simple/utils/simple-state.js
@@ -0,0 +1,151 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+const Dictionary = 'abcdefghijklmnopqrstuvwxyz';
+const dictionary = "0123456789abcdef";
+
+/**
+ * Class for managing simple account states.
+ */
+class SimpleState {
+
+    /**
+     * Initializes the instance.
+     */
+    constructor(workerIndex, initialMoney, moneyToTransfer, accounts = 0) {
+        this.accountsGenerated = accounts;
+        this.initialMoney = initialMoney;
+        this.moneyToTransfer = moneyToTransfer;
+        this.accountPrefix = this._get26Num(workerIndex);
+    }
+
+    /**
+     * Generate string by picking characters from the dictionary variable.
+     * @param {number} number Character to select.
+     * @returns {string} string Generated string based on the input number.
+     * @private
+     */
+    _get26Num(number){
+        let result = '';
+
+        while(number > 0) {
+            result += Dictionary.charAt(number % Dictionary.length);
+            number = parseInt(number / Dictionary.length);
+        }
+
+        return result;
+    }
+
+    /**
+     * Construct an account key from its index.
+     * @param {number} index The account index.
+     * @return {string} The account key.
+     * @private
+     */
+    _getAccountKey(index) {
+        return this.accountPrefix + this._get26Num(index);
+    }
+
+    /**
+     * Returns a random account key.
+     * @return {string} Account key.
+     * @private
+     */
+    _getRandomAccount() {
+        // choose a random TX/account index based on the existing range, and restore the account name from the fragments
+        const index = Math.ceil(Math.random() * this.accountsGenerated);
+        return this._getAccountKey(index);
+    }
+
+    getRandomAccount() {
+        let address = "0x";
+        for(let i = 0; i < 40; i++){
+            let index = Math.floor(Math.random() * 16);
+            address += dictionary.charAt(index);
+        }
+        return address;
+    }
+
+    /**
+     * Get the arguments for creating a new account.
+     * @returns {object} The account arguments.
+     */
+    getJoinNetworkArguments() {
+        this.accountsGenerated++;
+        return {
+            account: this.getRandomAccount()
+        };
+    }
+
+    getAddSubscriberArguments() {
+        return {
+            input: this.getRandomAccount(),
+            sub: this.getRandomAccount()
+        };
+    }
+
+    getGetSubscriberArguments(){
+        return{
+            account: this.getRandomAccount()
+        };
+    }
+
+    getUploadHashArguments() {
+        return {
+            cid: "abcdefghijklmnopqrstuvwxyz1234567890",
+            account: this.getRandomAccount(),
+            checksum: "abcdefghijklmnopqrstuvwxyz1234567890"
+        };
+    }
+
+    getGetHashArguments(){
+        return{
+            account: this.getRandomAccount()
+        };
+    }
+
+    getGetNotificationArguments(){
+        return{
+            account: this.getRandomAccount()
+        };
+    }
+}
+
+
+//     /**
+//      * Get the arguments for querying an account.
+//      * @returns {object} The account arguments.
+//      */
+//     getQueryArguments() {
+//         return {
+//             account: this._getRandomAccount()
+//         };
+//     }
+
+//     /**
+//      * Get the arguments for transfering money between accounts.
+//      * @returns {object} The account arguments.
+//      */
+//     getTransferArguments() {
+//         return {
+//             source: this._getRandomAccount(),
+//             target: this._getRandomAccount(),
+//             amount: this.moneyToTransfer
+//         };
+//     }
+// }
+
+module.exports = SimpleState;
